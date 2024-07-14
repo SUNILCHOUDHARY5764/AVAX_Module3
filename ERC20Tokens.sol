39310b82eee3f357c8623de2acca9f1eceebe8f3
Sunil_Chaudhary <148885891+SUNILCHOUDHARY5764@users.noreply.github.com>
2024-07-14 21:56:35 +0530

    Update ERC20Tokens.sol

--- ERC20Tokens.sol
+++ ERC20Tokens.sol
@@ -2,30 +2,25 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC20_Token is ERC20, Ownable {
-    uint256 public totalTokens = 0;
-
-    mapping(address => uint256) public balances;
+contract ERC20_Tokens is ERC20, ERC20Burnable, Ownable {
 
     constructor() ERC20("Token", "TKN") Ownable(msg.sender) {}
 
-    function mintTokens(address account, uint256 amount) public onlyOwner {
-        require(account==msg.sender,"Only owner can mint tokens");
-        totalTokens += amount;
-        balances[account]+= amount;
+    function mintTokens(address to, uint256 amount) public onlyOwner {
+       _mint(to, amount); 
     }
 
-    function burnTokens(uint256 amount) public {
-        require(balances[msg.sender]>=amount,"Isufficent Balance");
-        totalTokens -= amount;
-        balances[msg.sender] -= amount;
+    function transferTokens(address _receiver, uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        transferFrom(msg.sender, _receiver, amount);
     }
 
-    function transferTokens(address account, uint256 amount) public {
-        require(balances[msg.sender] >= amount, "Unsufficient balance");
-        balances[msg.sender] -= amount;
-        balances[account] += amount;
+    function burnTokens(uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender,amount);
     }
+
 }
